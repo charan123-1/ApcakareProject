58209425e6a186d3fe257ca7c2702903d6386057
Charan Sai Dasari <charansaidasari@gmail.com>
2021-07-13 14:12:17 +0530

    first commit

--- back-end/backendapi/api/urls.py
+++ back-end/backendapi/api/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from .views import UserViewSet
+
 router = routers.DefaultRouter()
 # to register users url --> to get to the users
-router.register('users', UserViewSet)
+
 urlpatterns = [
     path('', include(router.urls)),
 
